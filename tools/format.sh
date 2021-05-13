79cc72d45772333af1ae51f6011b42d378436c0f
Andrew Archibald <andrew@aarchibald.com>
2021-05-13 02:50:42 -0700

    Sort projects by decreasing date, then by name

--- tools/format.sh
+++ tools/format.sh
@@ -4,10 +4,10 @@
 # https://unix.stackexchange.com/questions/524048/sort-file-by-group-of-lines/524051#524051
 perl -0ne 'print sort split(/^(?=\S)/m)' .gitmodules | sponge .gitmodules
 
-# Format JSON and sort by date
+# Format JSON and sort by decreasing date, then name
 # npm install -g underscore-cli
 # https://github.com/ddopson/underscore-cli
 underscore print -i projects.json |
-  jq -c 'sort_by(.date) | reverse' |
+  jq -c '[group_by(.date)[] | sort_by(.name)] | sort_by(.[0].date) | reverse | map(.[])' |
   underscore print -o projects.json
 echo >> projects.json
