628ae6c70fc553df47c6bcb47d0e5fe1d0d33d49
Andrew Archibald <andrew@aarchibald.com>
2021-06-25 03:59:56 -0700

    Set wspace 0.2 and 0.3 dates from earliest times in source archives

--- tools/format_projects.sh
+++ tools/format_projects.sh
@@ -2,6 +2,6 @@
 
 # Format JSON and sort by decreasing date, then name
 underscore print -i projects.json |
-  jq -c '[group_by(.date)[] | sort_by(.name)] | sort_by(.[0].date) | reverse | map(.[])' |
+  jq -c '[group_by(.date)[] | sort_by(.name)] | sort_by(.[0].date) | reverse | map(.[]) | sort_by(.name == "wspace 0.2")' |
   underscore print |
   tools/sponge projects.json
