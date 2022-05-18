5347d54c3dccc068f19fd17357384b32437e080b
Andrew Archibald <andrew@aarchibald.com>
2022-05-17 20:46:14 -0600

    Authenticate with GitHub App instead of OAuth

--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.16
 
 require (
 	github.com/andrewarchi/browser v0.0.0-20210602185959-ae587407e71c
+	github.com/bradleyfalzon/ghinstallation/v2 v2.0.4
 	github.com/google/go-github/v44 v44.1.0
 	github.com/pelletier/go-toml v1.9.5
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
 	golang.org/x/sys v0.0.0-20220517195934-5e4e11fc645e
 )
--- tools/search/search.go
+++ tools/search/search.go
@@ -2,24 +2,27 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"net/http"
 	"time"
 
+	"github.com/bradleyfalzon/ghinstallation/v2"
 	"github.com/google/go-github/v44/github"
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/endpoints"
 )
 
-const MinRequestSpacing = 5 * time.Second
+const (
+	AppID             = 1
+	InstallationID    = 99
+	PrivateKeyFile    = "2022-05-17.private-key.pem"
+	MinRequestSpacing = 5 * time.Second
+)
 
 func main() {
 	ctx := context.Background()
 
-	httpClient, err := authenticate(ctx, "GITHUB_CLIENT_ID", "GITHUB_CLIENT_SECRET")
+	transport, err := ghinstallation.NewKeyFromFile(http.DefaultTransport, AppID, InstallationID, PrivateKeyFile)
 	try(err)
-	client := github.NewClient(httpClient)
+	client := github.NewClient(&http.Client{Transport: transport})
 
 	opt := &github.SearchOptions{
 		Sort:  "indexed",
@@ -30,35 +33,6 @@ func main() {
 	})
 	log.Printf("Got %d results\n", len(results))
 	try(err)
-}
-
-func authenticate(ctx context.Context, clientID, clientSecret string) (*http.Client, error) {
-	conf := &oauth2.Config{
-		ClientID:     clientID,
-		ClientSecret: clientSecret,
-		Endpoint:     endpoints.GitHub,
-		Scopes:       []string{},
-	}
-
-	// Redirect user to consent page to ask for permission for the scopes
-	// specified above.
-	url := conf.AuthCodeURL("state", oauth2.AccessTypeOffline)
-	fmt.Printf("Visit the URL to authorize:\n%v\n", url)
-
-	// Use the authorization code that is pushed to the redirect URL. Exchange
-	// will do the handshake to retrieve the initial access token. The HTTP Client
-	// returned by conf.Client will refresh the token as necessary.
-	fmt.Print("Input code from URL query parameters: ")
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		return nil, err
-	}
-	tok, err := conf.Exchange(ctx, code)
-	if err != nil {
-		return nil, err
-	}
-
-	return conf.Client(ctx, tok), nil
 }
 
 func searchPaged(ctx context.Context, client *github.Client, opt *github.SearchOptions,
@@ -82,10 +56,10 @@ func searchPaged(ctx context.Context, client *github.Client, opt *github.SearchO
 			if spacing < MinRequestSpacing {
 				spacing = MinRequestSpacing
 			}
-			log.Printf("[Page %d] %d results in %s; sleeping %s\n", opt.Page, len(results.CodeResults), respDuration, spacing)
+			log.Printf("[Page %d] %d results in %s; sleeping %s\n", opt.Page, len(results.CodeResults), respDuration.Round(time.Millisecond), spacing.Round(time.Millisecond))
 			time.Sleep(spacing)
 		} else {
-			log.Printf("[Page %d] %d results in %s; done\n", opt.Page, len(results.CodeResults), respDuration)
+			log.Printf("[Page %d] %d results in %s; done\n", opt.Page, len(results.CodeResults), respDuration.Round(time.Millisecond))
 			return allResults, nil
 		}
 		opt.Page = resp.NextPage
