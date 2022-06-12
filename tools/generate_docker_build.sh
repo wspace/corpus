9996f76ee0eb6e8d67931e2b1770f1899a206f6b
Andrew Archibald <andrew@aarchibald.com>
2022-06-12 17:33:24 -0600

    List Docker commands for given Dockerfiles; use /var/empty as dir

--- tools/generate_docker_build.sh
+++ tools/generate_docker_build.sh
@@ -1,12 +1,19 @@
 #!/bin/sh -e
 
-echo 'docker build -t wspace-corpus/haskell/edwinb-wspace-0.3 -f haskell/edwinb-wspace-0.3.dockerfile --platform linux/amd64 /dev/null'
-echo 'docker run -i -t --rm wspace-corpus/haskell/edwinb-wspace-0.3 hworld.ws'
-echo 'docker build -t wspace-corpus/rust -f tools/rust/Dockerfile /dev/null'
-echo 'docker run -i -t --rm wspace-corpus/rust'
+dockerfiles=${*:-*/Dockerfile */*.dockerfile}
 
-for f in */*.dockerfile; do
-  f="${f%.dockerfile}"
-  echo "docker build -t wspace-corpus/$f -f $f.dockerfile /dev/null"
-  echo "docker run -i -t --rm wspace-corpus/$f"
+echo 'docker build -t wspace-corpus/haskell/edwinb-wspace-0.3 -f haskell/edwinb-wspace-0.3.dockerfile --platform linux/amd64 /var/empty'
+echo 'docker run -i -t --rm wspace-corpus/haskell/edwinb-wspace-0.3 hworld.ws'
+
+get_tag() {
+  t="${1%.dockerfile}"
+  t="${t%/Dockerfile}"
+  echo "wspace-corpus/$t"
+}
+
+for f in $dockerfiles; do
+  echo "docker build -q -t $(get_tag "$f") -f $f /var/empty"
+done
+for f in $dockerfiles; do
+  echo "docker run -i -t --rm $(get_tag "$f")"
 done
